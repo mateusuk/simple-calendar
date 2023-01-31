befb9546c5b07047b3abf686fb24d1b9acaf8dc3
Mateus de oliveira <mateus.oliveira.uk@gmail.com>
2023-01-31 18:40:40 +0000

    textarea javascrip

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,31 @@
-var timeDisplayEl = $('#currentDay');
+var timeDisplay = $('#currentDay');
+const timeblocks = $('.row');
 
-function displayTime() {
-    var Now = moment().format('dddd, MMMM Do.');
-    timeDisplayEl.text(Now);
+
+
+
+var currentTime;
+//function to display time 
+function getTime() {
+    var Now = moment().format('dddd, MMMM Do [at] h:mm A');
+    currentTime = timeDisplay.text(Now);
   }
-  setInterval(displayTime, 1000);
+  setInterval(getTime, 1000);
+
+  if(currentTime){
+
+  }
+
+//loop through time block to give them a style
+timeblocks.each(function() {
+    const hour = parseInt($(this).find('.hour').text().slice(0, -2));
+    if (hour < currentTime) {
+      $(this).addClass('past');
+    } else if (hour === currentTime) {
+      $(this).addClass('present');
+    } else {
+      $(this).addClass('future');
+    }
+  });
+
+
