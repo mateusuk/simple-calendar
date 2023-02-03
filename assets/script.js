5064a7272d2d769135dece029432ec3033ff6451
Mateus de oliveira <mateus.oliveira.uk@gmail.com>
2023-02-03 01:42:45 +0000

    change hour format

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,7 @@ const timeblocks = $('.row');
 var currentTime = moment().hour();
 //function to display time 
 function getTime() {
-    var Now = moment().format('dddd, MMMM Do [at] h:mm A');
+    var Now = moment().format('dddd, MMMM Do [at] H:mm A');
     currentTime = timeDisplay.text(Now);
   }
   setInterval(getTime, 1000);
@@ -16,16 +16,17 @@ function getTime() {
 
 
 //loop through time block to give them a style
+
 timeblocks.each(function() {
-    const hour = parseInt($(this).find('.hour').text().slice(0, -2));
-    if (hour < currentTime) {
-      $(this).addClass('past');
-    } else if (hour === currentTime) {
-      $(this).addClass('present');
-    } else {
-      $(this).addClass('future');
-    }
-  });
+  const hour = parseInt($(this).find('.hour').text().slice(0, -2));
+  if (hour < currentTime) {
+    $(this).addClass('past');
+  } else if (hour === currentTime) {
+    $(this).addClass('present');
+  } else {
+    $(this).addClass('future');
+  }
+});
 
 
 
